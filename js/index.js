d28d9d3febcd618a95eab8e105133c85cdbbe337
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2025-04-22 13:13:22 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
+  const fadeElements = document.querySelectorAll(".fade-in");
+
+  const fadeInOnScroll = function () {
+    fadeElements.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (elementTop < windowHeight - 100) {
+        element.classList.add("visible");
+      }
+    });
+  };
+
+  window.addEventListener("scroll", fadeInOnScroll);
+  window.addEventListener("load", fadeInOnScroll);
+
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href");
+      if (targetId === "#") return;
+
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop - 70,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
